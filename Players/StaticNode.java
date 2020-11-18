bb60ca48355127b5006ce203b0416357fb479380
Jack Glaister <glaisterj@cardiff.ac.uk>
2020-11-18 12:26:46 +0000

    few minor error fixes

--- Players/StaticNode.java
+++ Players/StaticNode.java
@@ -1,11 +1,11 @@
 import java.util.List;
 
 public class StaticNode extends MMNode{
-    public SnakePlayer snakes;
+    public SnakePlayer Snakes;
     public String move;
     public List<StaticNode> Children;
-    public StaticNode(List<Snake> snakes){
-        SnakePlayer = snakes;
+    public StaticNode(List<SnakePlayer> snakes){
+        Snakes = snakes;
     }
     public void AddChild(StaticNode child){
         Children.add(child);
