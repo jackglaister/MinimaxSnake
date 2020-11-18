ff11c992e79a4fe661214c1640d946f3a1bc1896
Jack Glaister <glaisterj@cardiff.ac.uk>
2020-11-18 18:22:53 +0000

    new

--- Players/MMNode.java
+++ Players/MMNode.java
@@ -5,5 +5,5 @@ import java.util.List;
 public abstract class MMNode{
     public SnakePlayer[] Snakes;
     public List<MMNode> Children;
- }
->>>>>>> de04f51919c8285f516cb3f5c098ebf954765a95
+}
+
--- Players/StaticNode.java
+++ Players/StaticNode.java
@@ -5,7 +5,6 @@ public class StaticNode extends MMNode{
     public String move;
     public StaticNode(SnakePlayer[] snakes){
         Snakes = snakes;
-        Children = new List<MMNode>
     }
     public void AddChild(StaticNode child){
         Children.add(child);
